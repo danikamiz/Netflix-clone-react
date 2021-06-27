2402dcdf0e6d44013ee9c8c03ac011292b634741
danikamiz <danidelmoral12@gmail.com>
2021-06-28 01:34:29 +0200

    Second commit

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -6,12 +6,12 @@ import { seedDatabase } from '../seed';
     // Show database with hooks
     // we need config
 const config = {
-    apiKey: "AIzaSyAtBSLHED6OogZ0uek6TuskcN5DACEa26w",
-    authDomain: "futflix-7b08c.firebaseapp.com",
-    projectId: "futflix-7b08c",
-    storageBucket: "futflix-7b08c.appspot.com",
-    messagingSenderId: "188238854853",
-    appId: "1:188238854853:web:ffa827abd14840b5184eac"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 const firebase = Firebase.initializeApp(config);
